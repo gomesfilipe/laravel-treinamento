0c4a9208e0a906398bb8154a14c69995d2b29677
gomesfilipe <filipejvmf@gmail.com>
2023-09-11 12:12:15 -0300

    changing route urls and adding pattern for id

--- treinamento/routes/api.php
+++ treinamento/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\AddressController;
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\MyUserController;
 use Illuminate\Http\Request;
@@ -12,50 +11,51 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('ability:admin')->group(function () {
-        Route::get('/company', [CompanyController::class, 'index']);
-        Route::get('/company/{company}', [CompanyController::class, 'show']);
+        Route::get('/companies', [CompanyController::class, 'index']);
+        Route::get('/company/{id}', [CompanyController::class, 'show']);
         Route::post('/company', [CompanyController::class, 'store']);
-        Route::put('/company/{company}', [CompanyController::class, 'update']);
-        Route::delete('/company/{company}', [CompanyController::class, 'destroy']);
+        Route::put('/company/{id}', [CompanyController::class, 'update']);
+        Route::delete('/company/{id}', [CompanyController::class, 'destroy']);
         
-        // adicionar usuário em uma company ok
-        Route::post('/company/{company}/users/add/{user}', [CompanyController::class, 'addUserInCompany']);
+        // adicionar usuário em uma company
+        Route::post('/company/{company_id}/user/{user_id}', [CompanyController::class, 'addUserInCompany'])
+            ->where(['company_id', 'user_id'], '[0-9]+');
 
-        // listar usuários de uma empresa específica ok
-        Route::get('/company/{company}/users', [CompanyController::class, 'indexUsersFromCompany']);
+        // listar usuários de uma empresa específica
+        Route::get('/company/{id}/users', [CompanyController::class, 'indexUsersFromCompany']);
 
         // listas empresas de um usuário específico
-        Route::get('/myuser/{myuser}/companies', [MyUserController::class, 'indexCompaniesFromUser']);
+        Route::get('/user/{id}/companies', [MyUserController::class, 'indexCompaniesFromUser']);
         
-        Route::get('/admin/myuser', [MyUserController::class, 'index']);
-        Route::get('/myuser/{myuser}', [MyUserController::class, 'show']);
-        Route::put('/myuser/{myuser}', [MyUserController::class, 'update']);
-        Route::delete('/myuser/{myuser}', [MyUserController::class, 'destroy']);
-        Route::post('/admin/myuser', [MyUserController::class, 'storeAdmin']);
+        Route::get('/users', [MyUserController::class, 'index']);
+        Route::get('/user/{id}', [MyUserController::class, 'show']);
+        Route::put('/user/{id}', [MyUserController::class, 'update']);
+        Route::delete('/user/{id}', [MyUserController::class, 'destroy']);
+        Route::post('/admin-user', [MyUserController::class, 'storeAdmin']);
 
         // atualizar foto de perfil de outro user
-        Route::post('/upload/{myuser}', [MyUserController::class, 'uploadProfilePicture']);
+        Route::post('/user-update-profile-picture/{id}', [MyUserController::class, 'uploadProfilePicture']);
     });
     
     Route::middleware(('ability:client,admin'))->group(function () {
-        Route::get('/myuser', [MyUserController::class, 'showMe']);
-        Route::put('/myuser', [MyUserController::class, 'updateMe']);
+        Route::get('/my-user', [MyUserController::class, 'showMe']);
+        Route::put('/my-user', [MyUserController::class, 'updateMe']);
 
         // listar empresas do próprio usuário
-        Route::get('/myusercompanies', [MyUserController::class, 'indexMyCompanies']);
+        Route::get('/my-user-companies', [MyUserController::class, 'indexMyCompanies']);
         
         // atualizar foto de perfil
-        Route::post('/upload', [MyUserController::class, 'uploadMyProfilePicture']);
+        Route::post('/my-user-update-profile-picture', [MyUserController::class, 'uploadMyProfilePicture']);
     }); 
 
     // Route::get('/address', [AddressController::class, 'index']);
-    // Route::get('/address/{address}', [AddressController::class, 'show']);
+    // Route::get('/address/{id}', [AddressController::class, 'show']);
     // Route::post('/address', [AddressController::class, 'store']);
-    // Route::put('/address/{address}', [AddressController::class, 'update']);
-    // Route::delete('/address/{address}', [AddressController::class, 'destroy']);
+    // Route::put('/address/{id}', [AddressController::class, 'update']);
+    // Route::delete('/address/{id}', [AddressController::class, 'destroy']);
 });
 
-Route::post('/myuser', [MyUserController::class, 'storeClient']);
+Route::post('/client-user', [MyUserController::class, 'storeClient']);
 Route::post('/login', [MyUserController::class, 'login']); 
 
 // token admin
